867051065c4365620f8bfdaefb6b28365eff2915
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2024-01-03 20:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toCase(text) {
   // write your code here
+	let word = text.toLowerCase();
+	for(let i = 0; i<word.length;i++){
+		 console.log(`${word}+"-" ${text.toUpperCase()}`);
+	}
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
